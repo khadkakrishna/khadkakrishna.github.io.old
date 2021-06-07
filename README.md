d450ab39762ba55ecc59653f1b3fa53a5cd3baaf
khadkakrishna <krishnakhadka2802@gmail.com>
2021-06-07 19:21:19 +0530

    adding basic template with contact and connect me

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # khadkakrishna.github.io
+
+#### color codes
+#173f5f dark blue
+#20639b blue
+#3caea3 green
+#f6d55c yellow
+#ed553b red
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,31 @@
-/*!
-* Start Bootstrap - Coming Soon v6.0.0 (https://startbootstrap.com/theme/coming-soon)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-coming-soon/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+var array_name1=["I can do some " , "I can ", "I can take ", "I love ", "I can ", "Yes, I am a "]
+var array_name2=["amazing things" , "paint and draw", "good pictures", "adventures and travelling", "also cook", "Software Engineer..."]
+var i=-1;
+
+function taglineChange() {
+    document.getElementById("tagline1").innerHTML = nextItem1();
+    document.getElementById("tagline2").innerHTML = nextItem2();
+}
+
+function nextItem1() {
+    i = i + 1;
+    if(i>=array_name1.length)
+    {
+        $('button').prop('disabled', true);
+        i=5;
+    }   
+    return array_name1[i];
+}        
+function nextItem2() {
+    return array_name2[i];
+}
+
+//Click event handler for nav-items
+$('.nav-item').on('click',function(){
+
+    //Remove any previous active classes
+    $('.nav-item').removeClass('active');
+  
+    //Add active class to the clicked item
+    $(this).addClass('active');
+});
