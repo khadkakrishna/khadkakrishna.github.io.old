af6e71a125690e99215f0510b51100a89d18ebd9
khadkakrishna <krishnakhadka2802@gmail.com>
2021-06-09 10:42:36 +0530

    Adding javascript code for the bio-length radio button

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,29 @@ $('.nav-item').on('click',function(){
     //Add active class to the clicked item
     $(this).addClass('active');
 });
+
+//event handler for bio shorter or longer
+function bioChanger() {
+    if ( document.getElementById("length-shortest").checked){
+        document.getElementById("bio-extended-text").innerHTML = ".";
+    }
+    else if (document.getElementById("length-shorter").checked){
+        document.getElementById("bio-extended-text").innerHTML = ", and i try to create things in artistic ways."
+    }
+    else if (document.getElementById("length-short").checked){
+        document.getElementById("bio-extended-text").innerHTML = ", I believe that art can change the world. So i try to create things in artistic ways."
+    }
+    else if (document.getElementById("length-long").checked){
+        document.getElementById("bio-extended-text").innerHTML = ", I believe that art can change the world. So i try to create things in artistic ways. I spend most of my time in learning new technologies and reading about it."
+    }
+    else if (document.getElementById("length-longer").checked){
+        document.getElementById("bio-extended-text").innerHTML = ", I believe that art can change the world. So i try to create things in artistic ways. I spend most of my time in learning new technologies and reading about it. I believe in lifting each other and share what we learn."
+    }
+    else if (document.getElementById("length-longest").checked){
+        document.getElementById("bio-extended-text").innerHTML = ", I believe that art can change the world. So i try to create things in artistic ways.I spend most of my time in learning new technologies and reading about it. I give my best in developing effective and effecient softwares and solutions. I believe in lifting each other and share what we learn."
+    }
+    else
+    {
+        document.getElementById("bio-extended-text").innerHTML = ", I believe that art can change the world. So i try to create things in artistic ways. I spend most of my time in learning new technologies and reading about it.";
+    }
+}
